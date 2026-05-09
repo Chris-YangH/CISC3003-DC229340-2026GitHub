3cbc4c1d8651947d672bd2944c4edaa3ab08df55
Chris-YangH <dc22934@um.edu.mo>
2026-05-10 01:09:57 +0800

    Remove CISC3003-GROUP02-Project

--- CISC3003-GROUP02-Project/server/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-function requireLogin(req, res, next) {
-  if (!req.session || !req.session.userId) {
-    return res.status(401).json({ error: 'Authentication required' });
-  }
-  next();
-}
-
-module.exports = { requireLogin };
